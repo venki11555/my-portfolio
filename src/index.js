93f43dc6bdce586d5f49141fa9356c9d3351ebf0
Venkatesh <kvenkey35@gmail.com>
2026-04-02 00:52:14 +0530

    Updated portfolio website

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio.jsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Portfolio />
+    <App />
   </React.StrictMode>,
 );
